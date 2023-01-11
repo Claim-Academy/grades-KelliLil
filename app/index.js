397e5307b5c4897fab61c9f1f36f9f4847183407
Kelli Lilly <kelli.lilly05@gmail.com>
2023-01-11 14:35:46 -0600

    await apiService

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 import apiService from "./api.service.js";
 import { getAllStudentNames } from "./lib.js";
+const gradesData = await apiService.getStudent();
 
-console.log(getAllStudentNames);
+console.log(gradesData);
